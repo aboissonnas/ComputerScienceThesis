59f3457fef9a3487b3fc6f0ba6865e5004590c32
Ariane Boissonnas <ariane@boissonnas.net>
2017-12-10 19:41:57 -0600

    tried to get my work to upload to git by not adding all binary files

---
+++ Assets/Scripts/EarthRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthRotation : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//one real-world second will be one hour in the simulation
+		var degPerHour = 15;
+
+		//rotate around the local Y-axis
+		transform.Rotate(Vector3.up * degPerHour * Time.deltaTime);
+	}
+}
